0faa70d099c0578f57b370786c6243209cb493f4
EmmanuelPerezP <emmanuelperezp@gmail.com>
2018-05-16 15:17:43 -0600

    add comments and stuff

--- AvesCostaRica/AvesCostaRica/settings.py
+++ AvesCostaRica/AvesCostaRica/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CR'
 
 TIME_ZONE = 'UTC'
 
